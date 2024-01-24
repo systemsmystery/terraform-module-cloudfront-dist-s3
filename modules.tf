2d799e396147a6c4166a18a28799ce9e73b7d4a5
Richard Annand <richard+git@ohheyrj.co.uk>
2024-01-24 12:24:22 +0000

    fix: changed certificate_domain_name to use local

--- modules.tf
+++ modules.tf
@@ -2,7 +2,7 @@ module "cert" {
   # checkov:skip=CKV_TF_1
   source                  = "gitlab.com/systemsmystery/terraform-module-acm-certificates/aws"
   version                 = "1.0.0"
-  certificate_domain_name = "cdn.${var.domain_name}"
+  certificate_domain_name = local.cdn_domain
   zone_name               = data.aws_route53_zone.zone.name
   providers = {
     aws = aws.us_east_1
